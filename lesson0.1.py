508e6e6cdf5385a464e6cf480e3ccb42828d1daa
Sheeki1 <shekikiki333@gmail.com>
2024-11-04 11:40:02 +0300

    Add files via upload

---
+++ lesson0.1.py
@@ -0,0 +1,15 @@
+# 1st program
+print (9**0.5*5)
+#2st program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print (2*2+2)
+print (2*(2+2))
+print (2*2+2 == 2*(2+2))
+#4th program
+A="123.456"
+A = float(A)
+A = A*10
+A = int(A)
+A = A % 10
+print(A)
